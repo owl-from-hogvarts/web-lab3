f431268e5f5228e63c9068df4fdcd49c58bbb8ae
owl-from-hogvarts <transftank@mail.ru>
2023-09-02 12:47:21 +0300

    feat: implement axises

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ const canvas = document.querySelector("#plot") as HTMLCanvasElement;
 const context = canvas.getContext("2d")!;
 
 type Position = { x: number; y: number };
+type UnitMark = Position & {label: string, isHorizontal: boolean}
 
 const canvasWidth = canvas.clientWidth;
 const canvasHeight = canvas.clientHeight;
@@ -24,78 +25,119 @@ function drawAxis(
   context.save();
 
   const arrowBody = new Path2D();
-  arrowBody.moveTo(0, canvasHeight / 2);
-  arrowBody.lineTo(canvasWidth, canvasHeight / 2);
 
+
+  if (isVertical) {
+    const arrowTipPosition: Position = {
+      x: widthPercents(50),
+      y: 0
+    }
+    
+    arrowBody.moveTo(arrowTipPosition.x, arrowTipPosition.y)
+    arrowBody.lineTo(arrowTipPosition.x, canvasHeight)
+
+    context.save()
+    context.translate(arrowTipPosition.x, arrowTipPosition.y)
+    context.rotate(-Math.PI/2)
+    drawArrowTip(context)
+    context.restore()
+
+  } else {
+    const arrowTipPosition: Position = {
+      x: canvasWidth,
+      y: canvasHeight/2
+    }
+
+    arrowBody.moveTo(0, arrowTipPosition.y);
+    arrowBody.lineTo(arrowTipPosition.x, arrowTipPosition.y);
+
+    context.save()
+    context.translate(arrowTipPosition.x, arrowTipPosition.y)
+    drawArrowTip(context)
+    context.restore()
+  }
+
+  context.stroke(arrowBody);
+
+  const axisNumericSize = widthPercents(80);
+  const axisUnit = axisNumericSize / 2
+  const axisHalfUnit = axisUnit / 2
+  const unitMark: UnitMark = {
+    x: isVertical ? 0 : axisUnit,
+    y: isVertical ? axisUnit : 0,
+    label: "R",
+    isHorizontal: isVertical
+  };
+
+  const halfUnitMarker: UnitMark = {
+    x: isVertical ? 0 : axisHalfUnit,
+    y: isVertical ? axisHalfUnit : 0,
+    label: "R/2",
+    isHorizontal: isVertical
+  };
+
+  context.save()
+  context.translate(canvasWidth/2, canvasHeight/2)
+
+  drawUnitMark(context, unitMark);
+  drawUnitMark(context, halfUnitMarker);
+  
+  const mirrorAxis: keyof Position = isVertical ? "y" : "x"
+
+  const unitMarkNegative = {...unitMark, [mirrorAxis]: unitMark[mirrorAxis]*(-1)}
+  const halfUnitMarkerNegative = {...halfUnitMarker, [mirrorAxis]: halfUnitMarker[mirrorAxis]*(-1)}
+  
+  console.log(unitMarkNegative)
+  
+  drawUnitMark(context, unitMarkNegative)
+  drawUnitMark(context, halfUnitMarkerNegative)
+  
+  context.restore()
+  context.restore();
+}
+
+function drawArrowTip(context: CanvasRenderingContext2D) {
   const arrowTip = new Path2D();
   const arrowTipWidthPercents = 3;
   const arrowTipHalfHeightPercents = 1;
 
-  arrowTip.moveTo(canvasWidth, canvasHeight / 2);
+  arrowTip.moveTo(0,0)
   arrowTip.lineTo(
-    canvasWidth - widthPercents(arrowTipWidthPercents),
-    canvasHeight / 2 - heightPercents(arrowTipHalfHeightPercents)
-  );
-  
-  arrowTip.lineTo(
-    canvasWidth - widthPercents(arrowTipWidthPercents),
-    canvasHeight / 2 + heightPercents(arrowTipHalfHeightPercents)
+    -widthPercents(arrowTipWidthPercents),
+    -heightPercents(arrowTipHalfHeightPercents)
   );
 
-  if (isVertical) {
-    // context.translate(0, canvasHeight);
-    context.rotate(-Math.PI / 2);
-    console.log(context.getTransform())
-  }
+  arrowTip.lineTo(
+    -widthPercents(arrowTipWidthPercents),
+    heightPercents(arrowTipHalfHeightPercents)
+  );
 
-  context.stroke(arrowBody);
   context.fill(arrowTip);
-
-  const axisNumericSize = widthPercents(80)
-
-  const unitMark: Position = {
-    x: (canvasWidth + axisNumericSize)/2,
-    y: canvasHeight/2,
-  }
-
-  const halfUnitMarker: Position = {
-    x: (canvasWidth + axisNumericSize/2)/2,
-    y: canvasHeight/2
-  }
-
-  context.save()
-  // context.translate(canvasWidth/2, canvasHeight/2)
-  // context.rotate(Math.PI)
-  // context.translate(-canvasWidth/2, -canvasHeight/2)
-  context.translate(canvasWidth, 0)
-  context.scale(-1, 1)
-  context.fillRect(0,0,10,10)
-  drawUnitMark(context, unitMark)
-  drawUnitMark(context, halfUnitMarker)
-  
-  context.restore()
-
-  drawUnitMark(context, unitMark)
-  drawUnitMark(context, halfUnitMarker)
-
-  context.restore();
 }
 
-function drawUnitMark(context: CanvasRenderingContext2D, unitMark: Position, isHorizontal: boolean) {
-  const unitMarkHalfHeight = heightPercents(2)
-  const unitMarkHalfWidth = widthPercents(0.3)
+function drawUnitMark(
+  context: CanvasRenderingContext2D,
+  unitMark: UnitMark,
+) {
+  const unitMarkHalfHeight = heightPercents(1.8);
+  const unitMarkHalfWidth = widthPercents(0.1);
 
-  context.fillRect(unitMark.x - unitMarkHalfWidth, unitMark.y - unitMarkHalfHeight, unitMarkHalfWidth*2, unitMarkHalfHeight*2)
-
-  const textMargin = heightPercents(3)
+  const textMargin = heightPercents(3);
   context.save()
-  // console.log(transformation.e, transformation.f)
-  // context.setTransform(0, 0, 0, 0, transformation.e, transformation.f)
-  context.fillText("R", unitMark.x - unitMarkHalfWidth, unitMark.y + unitMarkHalfHeight + textMargin)
+  context.translate(unitMark.x, unitMark.y)
+  context.save()
+  context.rotate(unitMark.isHorizontal ? Math.PI / 2 : 0)
+  context.fillRect(
+    -unitMarkHalfWidth,
+    -unitMarkHalfHeight,
+    unitMarkHalfWidth * 2,
+    unitMarkHalfHeight * 2
+  );
   context.restore()
-
-  if (isHorizontal) {
-    
-  }
+  context.fillText(
+    unitMark.label,
+    unitMarkHalfWidth + (unitMark.isHorizontal ? textMargin : 0),
+    unitMarkHalfHeight + (!unitMark.isHorizontal ? textMargin : 0)
+  );
+  context.restore()
 }
-
