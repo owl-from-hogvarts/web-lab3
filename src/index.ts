5ec8e80fa79eb7dffd0ac9a4aef0d7567855ea27
owl-from-hogvarts <transftank@mail.ru>
2023-09-02 20:06:24 +0300

    feat: draw figure

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,64 @@ type UnitMark = Position & {label: string, isHorizontal: boolean}
 const canvasWidth = canvas.clientWidth;
 const canvasHeight = canvas.clientHeight;
 
+const AXIS_NUMERIC_SIZE = widthPercents(80);
+const AXIS_UNIT = AXIS_NUMERIC_SIZE / 2
+const AXIS_HALF_UNIT = AXIS_UNIT / 2
+
+const FIGURE_COLOR = "#3399ff"
+
+
+context.save()
+context.translate(canvasWidth/2, canvasHeight/2)
+context.save()
+drawLeftBottom(context)
+context.rotate(Math.PI/2)
+drawRightBottom(context)
+context.restore()
+context.rotate(-Math.PI/2)
+drawLeftTop(context)
+
+context.restore()
+
 drawAxis(context, false);
 drawAxis(context, true);
+
+
+
+function drawLeftTop(context: CanvasRenderingContext2D) {
+  context.save()
+  const triangle = new Path2D()
+  triangle.moveTo(0,0)
+  triangle.lineTo(0, AXIS_UNIT)
+  triangle.lineTo(AXIS_UNIT, 0)
+  
+  context.fillStyle = FIGURE_COLOR
+  context.fill(triangle)
+  context.restore();
+}
+
+function drawLeftBottom(context: CanvasRenderingContext2D) {
+  context.save()
+  const arc = new Path2D()
+  arc.moveTo(0,0)
+  arc.arc(0, 0, AXIS_HALF_UNIT, 0, Math.PI/2)
+
+  context.fillStyle = FIGURE_COLOR
+  context.fill(arc)
+  context.restore()
+}
+
+function drawRightBottom(context: CanvasRenderingContext2D) {
+  context.save()
+
+  const rect = new Path2D()
+  rect.moveTo(0,0)
+  rect.rect(0,0,AXIS_UNIT, AXIS_HALF_UNIT)
+
+  context.fillStyle = FIGURE_COLOR
+  context.fill(rect)
+  context.restore()
+}
 
 function widthPercents(percents: number): number {
   return (canvasWidth / 100) * percents;
@@ -59,19 +115,16 @@ function drawAxis(
 
   context.stroke(arrowBody);
 
-  const axisNumericSize = widthPercents(80);
-  const axisUnit = axisNumericSize / 2
-  const axisHalfUnit = axisUnit / 2
   const unitMark: UnitMark = {
-    x: isVertical ? 0 : axisUnit,
-    y: isVertical ? axisUnit : 0,
+    x: isVertical ? 0 : AXIS_UNIT,
+    y: isVertical ? AXIS_UNIT : 0,
     label: "R",
     isHorizontal: isVertical
   };
 
   const halfUnitMarker: UnitMark = {
-    x: isVertical ? 0 : axisHalfUnit,
-    y: isVertical ? axisHalfUnit : 0,
+    x: isVertical ? 0 : AXIS_HALF_UNIT,
+    y: isVertical ? AXIS_HALF_UNIT : 0,
     label: "R/2",
     isHorizontal: isVertical
   };
@@ -86,9 +139,7 @@ function drawAxis(
 
   const unitMarkNegative = {...unitMark, [mirrorAxis]: unitMark[mirrorAxis]*(-1)}
   const halfUnitMarkerNegative = {...halfUnitMarker, [mirrorAxis]: halfUnitMarker[mirrorAxis]*(-1)}
-  
-  console.log(unitMarkNegative)
-  
+    
   drawUnitMark(context, unitMarkNegative)
   drawUnitMark(context, halfUnitMarkerNegative)
   
